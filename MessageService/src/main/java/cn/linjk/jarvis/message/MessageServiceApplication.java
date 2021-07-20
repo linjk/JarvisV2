a4056a7ab7522e7293c710414380fa0163ada2cc
LinJK <linjk121@163.com>
2021-07-20 22:25:27 +0800

    add support 消息中心加Dubbo支持

--- MessageService/src/main/java/cn/linjk/jarvis/message/MessageServiceApplication.java
+++ MessageService/src/main/java/cn/linjk/jarvis/message/MessageServiceApplication.java
@@ -1,5 +1,6 @@
 package cn.linjk.jarvis.message;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date: 2021/7/20 上午12:25
  * @description:
  */
+@EnableDubbo
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MessageServiceApplication {
