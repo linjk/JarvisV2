6fa874fffb04cd059164d51986ea10ae40d8ba37
LinJK <linjk121@163.com>
2021-07-21 00:38:23 +0800

    配置文件动态刷新，通过

--- UserService/src/main/java/cn/linjk/jarvis/user/rest/UserRest.java
+++ UserService/src/main/java/cn/linjk/jarvis/user/rest/UserRest.java
@@ -2,6 +2,8 @@ package cn.linjk.jarvis.user.rest;
 
 import cn.linjk.jarvis.api.message.IMessageApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,9 +16,17 @@ import org.springframework.web.client.RestTemplate;
  * @date: 2021/7/20 上午12:42
  * @description:
  */
+@RefreshScope
 @RestController
 public class UserRest {
     @Autowired private IMessageApi messageApi;
+
+    @Value("${version: null}") private String version;
+
+    @GetMapping("/version")
+    public String getVersion() {
+        return version;
+    }
 
     @GetMapping("/user-test")
     public String getMessage() {
