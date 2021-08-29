f298184b07ef2823c83f78aa3c8c3e4ce4d032b8
LinJK <linjk121@163.com>
2021-08-29 23:03:02 +0800

    add 秒杀服务模块

---
+++ SkillService/src/main/java/cn/linjk/jarvis/skill/SkillServiceApplication.java
@@ -0,0 +1,19 @@
+package cn.linjk.jarvis.skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: SkillServiceApplication
+ * @author: linjk
+ * @date: 2021/8/29 下午10:47
+ * @description:
+ */
+@SpringBootApplication
+public class SkillServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillServiceApplication.class, args);
+    }
+}
