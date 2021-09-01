a996acb7ea66273b9c778e34293d2aa133c13061
LinJK <linjk121@163.com>
2021-09-02 00:42:44 +0800

    补充提交

---
+++ Modules/common/src/test/java/cn/linjk/jarvis/common/test/Constants.java
@@ -0,0 +1,18 @@
+package cn.linjk.jarvis.common.test;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: Constants
+ * @author: linjk
+ * @date: 2021/9/1 下午10:29
+ * @description:
+ */
+public interface Constants {
+    // 1亿条数据
+    int EVENT_NUM_OHM = 100000000;
+    // 5千万条数据
+    int EVENT_NUM_FM = 50000000;
+    // 1千万条数据
+    int EVENT_NUM_OM = 10000000;
+}
