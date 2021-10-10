9d086de9f558dd2ea81a73db2f7f02f0a9476b68
LinJK <linjk121@163.com>
2021-10-10 16:52:30 +0800

    移除mqtt头文件包含

--- native/main.cpp
+++ native/main.cpp
@@ -6,7 +6,7 @@
 //#include "utils/mysql_connector.hpp"
 //#include "utils/car_oil_util.hpp"
 #include "utils/json_util.hpp"
-#include "utils/mqtt_util.hpp"
+//#include "utils/mqtt_util.hpp"
 
 int main(int argc, char* argv[]) {
     if (2 == argc) {
