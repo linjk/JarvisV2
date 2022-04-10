052222a8b913efcfb7992b07b770bdcb471c34ac
LinJK <linjk121@163.com>
2022-04-10 21:58:52 +0800

    模板化

--- native/utils/selection_sort_util.hpp
+++ native/utils/selection_sort_util.hpp
@@ -7,9 +7,10 @@
 
 #include "../commons/jarvis.h"
 
+template<typename T>
 class selection_sort_util {
 public:
-    static void selectionSort(int arr[], int n) {
+    static void selectionSort(T arr[], int n) {
         for (int i = 0; i < n; i++) {
             // 寻找[i, n)区间里的最小值
             int minIndex = i;
@@ -26,8 +27,14 @@ public:
 TEST(selection_sort_util, selection_sort_util_test) { /* NOLINT */
     cout << "Test: selection_sort_util_test start" << endl;
     int a[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    selection_sort_util::selectionSort(a, 10);
+    selection_sort_util<int>::selectionSort(a, 10);
     for(int i : a) {
+        cout << i << " ";
+    }
+    cout << endl;
+    float b[5] = {1.2, 3.4, 0.8, 6.9, 11.23};
+    selection_sort_util<float>::selectionSort(b, 5);
+    for(float i : b) {
         cout << i << " ";
     }
     cout << endl;
