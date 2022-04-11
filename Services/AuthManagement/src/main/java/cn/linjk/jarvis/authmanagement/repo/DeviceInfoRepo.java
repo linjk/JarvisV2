8325c9c4efcb99920a8bda5e0011d52fa7efff80
LinJK <linjk121@163.com>
2022-04-12 00:21:20 +0800

    DeviceInfoRepo

---
+++ Services/AuthManagement/src/main/java/cn/linjk/jarvis/authmanagement/repo/DeviceInfoRepo.java
@@ -0,0 +1,20 @@
+package cn.linjk.jarvis.authmanagement.repo;
+
+import cn.linjk.jarvis.common.tables.DeviceInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: DeviceInfoRepo
+ * @author: linjk
+ * @date: 2022/4/11 下午10:03
+ * @description:
+ */
+@Repository
+public interface DeviceInfoRepo extends JpaRepository<DeviceInfo, Long> {
+    @Query(value = "select * from device_info where device_name = ?1", nativeQuery = true)
+    DeviceInfo findDeviceByDeviceNameame(String deviceName);
+}
