f746072dd156660dab55da326a870f0ad9cc00b1
LinJK <linjk121@163.com>
2022-04-16 21:32:09 +0800

    fix

--- Services/DeviceService/src/main/java/cn/linjk/jarvis/device/DeviceServiceApplication.java
+++ Services/DeviceService/src/main/java/cn/linjk/jarvis/device/DeviceServiceApplication.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Copyright 1990-2020 LinJK
@@ -16,10 +17,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDubbo
 @EnableDiscoveryClient
 @SpringBootApplication
-//@ComponentScan(basePackages = {
-//        "cn.linjk.jarvis.user"
-//        //"cn.linjk.jarvis.journal"
-//})
+@ComponentScan(basePackages = {
+        "cn.linjk.jarvis.device",
+        "cn.linjk.jarvis.common"
+})
 public class DeviceServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeviceServiceApplication.class, args);
