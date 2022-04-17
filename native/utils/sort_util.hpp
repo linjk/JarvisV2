63d227b3d36da32022bc0c32d14b409791a9ad23
LinJK <linjk121@163.com>
2022-04-17 10:11:56 +0800

    归并排序

--- native/utils/sort_util.hpp
+++ native/utils/sort_util.hpp
@@ -46,6 +46,15 @@ public:
         }
     }
 
+    /**
+     * 归并排序，复杂度：O(N*logN)
+     * @param arr 数组
+     * @param n 数组大小
+     */
+    static void mergeSort(T arr[], int n) {
+        m_merge_sort(arr, 0, n-1);
+    }
+
     static T* generateRandomArray(int num, int range_left, int range_right) {
         assert(range_left <= range_right);
 
@@ -101,9 +110,51 @@ public:
         clock_t end = clock();
         cout << "SortName: " << sort_name << ", cost " << double(end-start)/CLOCKS_PER_SEC << "s" << endl;
         // make sure you test is PASS
-        for(int i  = 0; i < num-1; i++) {
-            if (arr[i] > arr[i+1]) {
-                cerr << sort_name << " FAILED" << endl;
+//        for(int i  = 0; i < num-1; i++) {
+//            if (arr[i] > arr[i+1]) {
+//                cerr << sort_name << " FAILED" << endl;
+//            }
+//        }
+    }
+
+private:
+    // 对arr[start...end]的范围进行排序
+    static void m_merge_sort(int arr[], int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int middle_position = (start + end) / 2;
+        m_merge_sort(arr, start, middle_position);
+        m_merge_sort(arr, middle_position+1, end);
+        m_merge(arr, start, middle_position, end);
+    }
+
+    // 对arr[start...middle_position]和arr[middle_position+1...end]两部分数组进行归并
+    static void m_merge(int arr[], int start, int middle_position, int end) {
+        T temp[end-start+1];
+        for (int i = start; i <= end; i++) {
+            temp[i-start] = arr[i];
+        }
+
+        int i = start;
+        int j = middle_position + 1;
+        for (int k = start; k <= end; k++) {
+            if (i > middle_position) {
+                arr[k] = temp[j-start];
+                j++;
+            }
+            else if (j > end) {
+                arr[k] = temp[i-start];
+                i++;
+            }
+            else if (temp[i-start] < temp[j-start]) {
+                arr[k] = temp[i-start];
+                i++;
+            }
+            else {
+                arr[k] = temp[j-start];
+                j++;
             }
         }
     }
@@ -115,12 +166,18 @@ TEST(sort_util, sort_util_test) { /* NOLINT */
     cout << "arr_size: " << arr_size << endl;
     int* a = sort_util<int>::generateRandomArray(arr_size, 0, arr_size);
     int* b = sort_util<int>::copy_arr(a, arr_size);
+    int* c = sort_util<int>::copy_arr(a, arr_size);
 
     sort_util<int>::benchmark("选择排序", sort_util<int>::selectionSort, a, arr_size);
     sort_util<int>::benchmark("插入排序", sort_util<int>::insertionSort, b, arr_size);
+    sort_util<int>::benchmark("归并排序", sort_util<int>::mergeSort, c, arr_size);
+    sort_util<int>::print_arr(a, arr_size);
+    sort_util<int>::print_arr(b, arr_size);
+    sort_util<int>::print_arr(c, arr_size);
 
     delete[] a;
     delete[] b;
+    delete[] c;
     cout << "Test: sort_util_test end" << endl;
 }
 
