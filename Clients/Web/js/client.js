779fcd53b473011c1f9f4b06471f488cb8369efc
LinJK <linjk121@163.com>
2022-04-17 21:25:27 +0800

    抓拍并保存

--- Clients/Web/js/client.js
+++ Clients/Web/js/client.js
@@ -4,6 +4,12 @@ let localVideo  = document.querySelector('video');
 let audioSource = document.querySelector("select#audioSource");
 let audioOutput = document.querySelector("select#audioOutput");
 let videoSource = document.querySelector("select#videoSource");
+//picture
+let snapshot = document.querySelector('button#snapshot');
+let picture = document.querySelector('canvas#picture');
+let savePicture = document.querySelector('button#save');
+picture.width = 640;
+picture.height = 480;
 
 const mediaStreamContrains = {
     video: {
@@ -19,47 +25,60 @@ const mediaStreamContrains = {
     }
 };
 
-//判断浏览器是否支持这些 API
-if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-    console.log("enumerateDevices() not supported.");
-}
-else {
-    navigator.mediaDevices.enumerateDevices()
-        .then(function(deviceInfos) {
-            deviceInfos.forEach(function(deviceInfo) {
-                console.log(deviceInfo.kind + ": label = " + deviceInfo.label + ": id = " + deviceInfo.deviceId + ": groupId = " + deviceInfo.groupId);
+function start() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+    }
+    else {
+        navigator.mediaDevices.getUserMedia(mediaStreamContrains).then(function(mediaStream) {
+            window.stream = mediaStream;
+            localVideo.srcObject = mediaStream;
+        }).catch(handleError);
 
-                let option = document.createElement('option');
-                option.text = deviceInfo.label;
-                option.value = deviceInfo.deviceId;
-                if(deviceInfo.kind === 'audioinput'){
-                    audioSource.appendChild(option);
-                }
-                else if(deviceInfo.kind === 'audiooutput'){
-                    audioOutput.appendChild(option);
-                }
-                else if(deviceInfo.kind === 'videoinput'){
-                    videoSource.appendChild(option);
-                }
-            });
-        })
-        .catch(handleError);
-}
+        navigator.mediaDevices.enumerateDevices()
+            .then(function(deviceInfos) {
+                deviceInfos.forEach(function(deviceInfo) {
+                    console.log(deviceInfo.kind + ": label = " + deviceInfo.label + ": id = " + deviceInfo.deviceId + ": groupId = " + deviceInfo.groupId);
 
-navigator.mediaDevices.getUserMedia(mediaStreamContrains).then(
-    gotLocalMediaStream
-).catch(
-    handleLocalMediaStreamError
-);
-
-function gotLocalMediaStream(mediaStream){
-    localVideo.srcObject = mediaStream;
-}
-
-function handleLocalMediaStreamError(error){
-    console.log('navigator.getUserMedia error: ', error);
+                    let option = document.createElement('option');
+                    option.text = deviceInfo.label;
+                    option.value = deviceInfo.deviceId;
+                    if(deviceInfo.kind === 'audioinput'){
+                        audioSource.appendChild(option);
+                    }
+                    else if(deviceInfo.kind === 'audiooutput'){
+                        audioOutput.appendChild(option);
+                    }
+                    else if(deviceInfo.kind === 'videoinput'){
+                        videoSource.appendChild(option);
+                    }
+                });
+            })
+            .catch(handleError);
+    }
 }
 
 function handleError(err){
     console.log(err.name + " : " + err.message);
 }
+
+snapshot.onclick = function() {
+    picture.getContext('2d').drawImage(localVideo, 0, 0, picture.width, picture.height);
+}
+
+function downLoad(url){
+    let oA = document.createElement("a");
+    oA.download = 'photo';// 设置下载的文件名，默认是'下载'
+    oA.href = url;
+    document.body.appendChild(oA);
+    oA.click();
+    oA.remove(); // 下载之后把创建的元素删除
+}
+
+savePicture.onclick = function (){
+    downLoad(picture.toDataURL("image/jpeg"));
+}
+
+// -----------------------------------------------------------------------------
+
+start();
