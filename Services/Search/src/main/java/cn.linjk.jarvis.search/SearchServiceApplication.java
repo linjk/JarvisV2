858987f0018f2f72c530fcb53fe59b069361e43f
LinJK <linjk121@163.com>
2022-05-29 02:13:47 +0800

    增加搜索模块，未实现

---
+++ Services/Search/src/main/java/cn.linjk.jarvis.search/SearchServiceApplication.java
@@ -0,0 +1,19 @@
+package cn.linjk.jarvis.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: SearchServiceApplication
+ * @author: linjk
+ * @date: 2022/5/22 下午4:57
+ * @description:
+ */
+@SpringBootApplication
+public class SearchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class, args);
+    }
+}
