75cbf55c961771efad4c11bd18a192a866b023e2
LinJK <linjk121@163.com>
2022-05-29 00:40:20 +0800

    add 全局异常处理类

---
+++ Modules/common/src/main/java/cn/linjk/jarvis/common/bean/ParameterException.java
@@ -0,0 +1,38 @@
+package cn.linjk.jarvis.common.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: ParameterException
+ * @author: linjk
+ * @date: 2022/5/29 上午12:31
+ * @description:
+ */
+@Getter
+@Setter
+public class ParameterException extends RuntimeException {
+    private Integer errorCode;
+
+    public ParameterException() {
+        super(Constant.ERROR_MESSAGE);
+        this.errorCode = Constant.ERROR_CODE;
+    }
+
+    public ParameterException(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ParameterException(String message) {
+        super(message);
+        this.errorCode = Constant.ERROR_CODE;
+    }
+
+    public ParameterException(Integer errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+}
---
+++ Modules/common/src/main/java/cn/linjk/jarvis/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package cn.linjk.jarvis.common.exception;
+
+import cn.linjk.jarvis.common.bean.Constant;
+import cn.linjk.jarvis.common.bean.ParameterException;
+import cn.linjk.jarvis.common.bean.ResultInfo;
+import cn.linjk.jarvis.common.util.ResultInfoUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * Copyright 1990-2020 LinJK
+ *
+ * @fileName: GlobalExceptionHandler
+ * @author: linjk
+ * @date: 2022/5/29 上午12:29
+ * @description:
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @Resource private HttpServletRequest request;
+
+    @ExceptionHandler(ParameterException.class)
+    public ResultInfo<Map<String, String>> handlerParameterException(ParameterException ex) {
+        String path = request.getRequestURI();
+        ResultInfo<Map<String, String>> resultInfo = ResultInfoUtil.buildError(ex.getErrorCode(), ex.getMessage(), path);
+        return resultInfo;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResultInfo<Map<String, String>> handlerException(Exception ex) {
+        log.info("未知异常：{}", ex);
+        String path = request.getRequestURI();
+        ResultInfo<Map<String, String>> resultInfo = ResultInfoUtil.buildError(Constant.ERROR_CODE, ex.getMessage(), path);
+        return resultInfo;
+    }
+}
