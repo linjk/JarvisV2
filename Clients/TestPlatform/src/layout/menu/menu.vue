f3c17e96d37ecb8054bb1102b20b73867db0a701
LinJK <linjk121@163.com>
2022-06-11 22:21:05 +0800

    侧边导航引入按钮

--- Clients/TestPlatform/src/layout/menu/menu.vue
+++ Clients/TestPlatform/src/layout/menu/menu.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import menuLogoVue from './menuLogo.vue';
+import MenuItem from './menuItem.vue';
 </script>
 
 <template>
@@ -11,32 +12,7 @@ import menuLogoVue from './menuLogo.vue';
         default-active="2"
         text-color="#fff"
         class="el-menu-vertical-demo">
-        <el-sub-menu index="1">
-            <template #title>
-                <el-icon><location /></el-icon>
-                <span>系统管理</span>
-            </template>
-            <el-menu-item index="1-1">系统维护</el-menu-item>
-            <el-menu-item index="1-2">系统日志</el-menu-item>
-        </el-sub-menu>
-
-        <el-sub-menu index="2">
-            <template #title>
-                <el-icon><icon-menu /></el-icon>
-                <span>接口管理</span>
-            </template>
-            <el-menu-item index="2-1">接口维护</el-menu-item>
-            <el-menu-item index="2-2">接口测试</el-menu-item>
-            <el-menu-item index="2-3">接口日志</el-menu-item>
-        </el-sub-menu>
-        
-        <el-sub-menu index="2">
-            <template #title>
-                <el-icon><icon-menu /></el-icon>
-                <span>系统审计</span>
-            </template>
-            <el-menu-item index="3-1">操作记录</el-menu-item>
-        </el-sub-menu>
+        <MenuItem></MenuItem>
     </el-menu>
 </template>
 
--- Clients/TestPlatform/src/main.ts
+++ Clients/TestPlatform/src/main.ts
@@ -3,5 +3,12 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router'
+import * as Icons from '@element-plus/icons-vue'
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+const app = createApp(App)
+app.use(router).use(ElementPlus).mount('#app')
+
+// 遍历Icons，以组件形式注册到app
+Object.keys(Icons).forEach((key) => {
+    app.component(key, Icons[key as keyof typeof Icons])
+})
