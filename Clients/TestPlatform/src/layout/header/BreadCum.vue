cad3fd126491320a6f28a17f08158a48932566df
LinJK <linjk121@163.com>
2022-06-23 00:12:50 +0800

    面包屑导航功能完成

--- Clients/TestPlatform/src/layout/header/BreadCum.vue
+++ Clients/TestPlatform/src/layout/header/BreadCum.vue
@@ -1,11 +1,31 @@
 <script lang="ts" setup>
+import { Ref, ref, watch } from 'vue'
+import { useRoute, RouteLocationMatched } from 'vue-router'
+
+// 获取当前路由信息
+const route = useRoute();
+
+const tabs : Ref<RouteLocationMatched[]> = ref([]);
+
+const getBreadCum = () => {
+  let matched = route.matched.filter((item) => item.meta && item.meta.title);
+  const first = matched[0]
+  if (first.path !== '/dashboard') {
+    matched = [{path: '/dashboard', meta: {'title': '首页'}} as any ].concat(matched)
+  }
+  tabs.value = matched;
+}
+
+getBreadCum();
+
+watch(() => route.path, () => getBreadCum());
 </script>
 
 <template>
     <el-breadcrumb separator="/">
-        <el-breadcrumb-item >首页</el-breadcrumb-item>
-        <el-breadcrumb-item>系统管理</el-breadcrumb-item>
-        <el-breadcrumb-item>系统维护</el-breadcrumb-item>
+        <el-breadcrumb-item v-for="item in tabs">
+          {{item.meta.title}}
+        </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
--- Clients/TestPlatform/src/router/index.ts
+++ Clients/TestPlatform/src/router/index.ts
@@ -9,7 +9,10 @@ const routes : Array<RouteRecordRaw> = [
         children: [
             {
                 path: '/dashboard',
-                component: () => import('../layout/views/index/dashboard.vue')
+                component: () => import('../layout/views/index/dashboard.vue'),
+                meta: {
+                    title: '首页' 
+                }
             }
         ]       
     },
@@ -18,14 +21,23 @@ const routes : Array<RouteRecordRaw> = [
         path: '/system',
         component: layout,
         name: 'System',
+        meta: {
+            title: '系统管理' 
+        },
         children: [
             {
                 path: '/system/manage',
-                component: () => import('../layout/views/systems/SystemManagement.vue')
+                component: () => import('../layout/views/systems/SystemManagement.vue'),
+                meta: {
+                    title: '系统维护' 
+                }
             },
             {
                 path: '/system/manageLog',
-                component: () => import('../layout/views/systems/SystemManageLogs.vue')
+                component: () => import('../layout/views/systems/SystemManageLogs.vue'),
+                meta: {
+                    title: '系统日志' 
+                }
             }
         ]
     },
@@ -35,18 +47,30 @@ const routes : Array<RouteRecordRaw> = [
         path: '/interface',
         component: layout,
         name: 'Interface',
+        meta: {
+            title: '接口管理' 
+        },
         children: [
             {
                 path: '/interface/manage',
-                component: () => import('../layout/views/interfaces/InterfaceManagement.vue')
+                component: () => import('../layout/views/interfaces/InterfaceManagement.vue'),
+                meta: {
+                    title: '接口维护' 
+                }
             },
             {
                 path: '/interface/test',
-                component: () => import('../layout/views/interfaces/InterfaceTest.vue')
+                component: () => import('../layout/views/interfaces/InterfaceTest.vue'),
+                meta: {
+                    title: '接口测试' 
+                }
             },
             {
                 path: '/interface/log',
-                component: () => import('../layout/views/interfaces/InterfaceTestLog.vue')
+                component: () => import('../layout/views/interfaces/InterfaceTestLog.vue'),
+                meta: {
+                    title: '接口日志' 
+                }
             }
         ]
     }
