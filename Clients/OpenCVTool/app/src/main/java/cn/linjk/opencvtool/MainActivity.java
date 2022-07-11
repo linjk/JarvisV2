09c2b6d125b759163a67a0b55b042c49fa62254e
LinJK <linjk121@163.com>
2022-07-12 00:07:19 +0800

    android物理设备运行成功

--- Clients/OpenCVTool/app/src/main/java/cn/linjk/opencvtool/MainActivity.java
+++ Clients/OpenCVTool/app/src/main/java/cn/linjk/opencvtool/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText("OpenCVTool");
     }
 
     /**
