b23eb33ce9c5600bfb45f2d8f6a05351b6c1d1b0
LinJK <linjk121@163.com>
2022-07-12 23:52:26 +0800

    图形灰度测试成功

--- Clients/OpenCVTool/app/src/main/java/cn/linjk/opencvtool/MainActivity.java
+++ Clients/OpenCVTool/app/src/main/java/cn/linjk/opencvtool/MainActivity.java
@@ -2,10 +2,22 @@ package cn.linjk.opencvtool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,9 +29,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initOpenCvLibs();
+
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText("OpenCVTool");
+        tv.setText("OpenCV: " + Core.getVersionString());
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),  R.mipmap.me_sea, options);
+        Mat src = new Mat();
+        Mat dst = new Mat();
+        Utils.bitmapToMat(bitmap, src);
+        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGRA2GRAY);
+        Utils.matToBitmap(dst, bitmap);
+        ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    private void initOpenCvLibs() {
+        if (OpenCVLoader.initDebug()) {
+            Log.i("opencv", "load opencv libs success...");
+        }
     }
 
     /**
