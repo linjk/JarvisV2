9d8f4d614b2b34a9f13ffb8ceb77a41357fd9a68
LinJK <linjk121@163.com>
2022-12-13 00:39:27 +0800

    redislock

--- Modules/common/src/main/java/cn/linjk/jarvis/common/config/BeanConfig.java
+++ Modules/common/src/main/java/cn/linjk/jarvis/common/config/BeanConfig.java
@@ -1,9 +1,13 @@
 package cn.linjk.jarvis.common.config;
 
+import cn.linjk.jarvis.common.bean.RedisLock;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * Copyright 1990-2020 LinJK
@@ -15,9 +19,16 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class BeanConfig {
+    @Resource private RedisTemplate redisTemplate;
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RedisLock redisLock() {
+        return new RedisLock(redisTemplate);
     }
 }
