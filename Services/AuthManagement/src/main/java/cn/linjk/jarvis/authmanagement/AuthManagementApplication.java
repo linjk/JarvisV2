c9b9e78aae7d7f137cc5da6bfd6f9d6c11ff8d60
LinJK <linjk121@163.com>
2022-12-26 23:53:54 +0800

    mybatis扫描启动

--- Services/AuthManagement/src/main/java/cn/linjk/jarvis/authmanagement/AuthManagementApplication.java
+++ Services/AuthManagement/src/main/java/cn/linjk/jarvis/authmanagement/AuthManagementApplication.java
@@ -2,9 +2,8 @@ package cn.linjk.jarvis.authmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Copyright 1990-2020 LinJK
@@ -14,14 +13,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @date: 2022/4/11 下午8:50
  * @description:
  */
+@MapperScan(basePackages = {"cn.linjk.jarvis.common.mybatis.mapper"})
 @EnableDiscoveryClient
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "cn.linjk.jarvis.common.tables"
-})
-@EnableJpaRepositories(basePackages={
-        "cn.linjk.jarvis.repos"
-})
 public class AuthManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthManagementApplication.class, args);
